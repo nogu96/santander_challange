5b5f57358ddf0983ed008ce89c33f1dd14459927
Franco Alonso <falonzo@eudaimonia.com.ar>
2021-12-05 14:15:31 -0300

    dependencies

---
+++ app/src/main/java/com/example/falonzo/santander_challenge/App.kt
@@ -0,0 +1,17 @@
+package com.example.falonzo.santander_challenge
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules()
+        }
+    }
+}
